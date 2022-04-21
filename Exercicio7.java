9f706b472765790ca452fd5e93ae4384e93cbf51
Gustavo Rodrigues <102631537+GuuhRodrigues@users.noreply.github.com>
2022-04-21 00:17:35 -0300

    Add files via upload
    Escreva  um  algoritmo  que  declare  duas  variáveis  inteiras,  atribua  o  valor  30  a  primeira  e  15 
    para a segunda. Imprima o resultado da subtração e da multiplicação desses números.

---
+++ Exercicio7.java
@@ -0,0 +1,16 @@
+public class Exercicio7{
+	public static void main(String[] args) {
+
+		int valA, valB, valSub, valMult;
+
+		valA=30;
+		valB=15;
+
+		valSub=valA-valB;
+		valMult=valA*valB;
+
+		System.out.println("O valor da subtração é " + valA + " e o valor da multiplicação é " + valMult);
+
+
+	}
+}
